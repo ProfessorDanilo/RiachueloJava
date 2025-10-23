6034f2695fdd7679644bc56ca961ba53ff98aa70
Danilo Lima <djkcond@gmail.com>
2025-10-23 15:20:39 -0300

    arquivos migrados pois deve ser outro diretório para ser clonado

--- ecommerce/E_Commerce/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO produto (nome, descricao, preco, estoque) VALUES
-                                                          ('Notebook Dell', 'Notebook Dell Inspiron 15', 3500.00, 10),
-                                                          ('Smartphone Samsung', 'Samsung Galaxy S21', 2500.00, 25),
-                                                          ('Mouse Logitech', 'Mouse sem fio Logitech', 150.00, 100);
